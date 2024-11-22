080f09ae61043ab28f595f9cffdb1972e95c2d02
maryamhassan <maryamo.hassan@mail.utoronto.ca>
2024-11-22 16:35:54 -0500

    Modified FilterCategoryView, and implemented FilterCategoryState and FilterCategoryViewModel

--- src/main/java/view/FilterCategoryView.java
+++ src/main/java/view/FilterCategoryView.java
@@ -1,34 +1,34 @@
 package view;
 
+import interface_adapter.filter_category.FilterCategoryViewModel;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
+import java.util.logging.Filter;
 
 public class FilterCategoryView extends JPanel implements ActionListener, PropertyChangeListener {
 
     private final String viewName = "filter category view";
     private String categoryName;
-  //  private final FilterCategoryViewModel filterCategoryViewModel;
+    private final FilterCategoryViewModel filterCategoryViewModel;
     private String[] categoryOptions;
 
-    public FilterCategoryView(String category, String[] optionsList) {
-      this.categoryName = category;
-      this.categoryOptions = optionsList;
+    public FilterCategoryView(FilterCategoryViewModel filterCategoryViewModel) {
+      this.filterCategoryViewModel = filterCategoryViewModel;
+      this.categoryName = this.filterCategoryViewModel.getState().getCategoryName();
+      this.categoryOptions = this.filterCategoryViewModel.getState().getCategoryOptions();
       this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-      JLabel title = new JLabel(this.categoryName);
+      JLabel title = new JLabel(this.categoryName, SwingConstants.CENTER);
       this.add(title);
-
-      JScrollPane filterBox = new JScrollPane();
-      JList filterList = new JList<>();
-      filterList.setLayout(new BoxLayout(filterList, BoxLayout.PAGE_AXIS));
 
       for (String option : this.categoryOptions) {
         JCheckBox addedFilter = new JCheckBox(option);
-        filterList.add(addedFilter);
+        this.add(addedFilter);
         addedFilter.addActionListener(
                 new ActionListener() {
                   @Override
@@ -38,9 +38,6 @@ public class FilterCategoryView extends JPanel implements ActionListener, Proper
                 }
         );
       }
-      filterBox.add(filterList);
-      this.add(filterBox);
-
       JButton back = new JButton("Back to All Filters");
       this.add(back);
     }
@@ -54,4 +51,8 @@ public class FilterCategoryView extends JPanel implements ActionListener, Proper
     public void propertyChange(PropertyChangeEvent evt) {
 
     }
+
+  public String getViewName() {
+    return this.viewName;
+  }
 }
