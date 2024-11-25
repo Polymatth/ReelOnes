baf6f8aa2d4bcb6a5521267d66605f5adee04341
maryamhassan <maryamo.hassan@mail.utoronto.ca>
2024-11-24 21:50:33 -0500

    Implemented Filter Category Selection use case and began implementing Filter Application use case

--- src/main/java/interface_adapter/filter_category/FilterCategoryController.java
+++ src/main/java/interface_adapter/filter_category/FilterCategoryController.java
@@ -1,6 +1,10 @@
 package interface_adapter.filter_category;
 
 import java.util.List;
+import java.util.Map;
+
+import use_case.filter_application.FilterApplicationInputBoundary;
+import use_case.filter_application.FilterApplicationInputData;
 import use_case.filter_category_selection.FilterCategorySelectionInputData;
 import use_case.filter_category_selection.FilterCategorySelectionInputBoundary;
 import entity.Movie;
@@ -11,14 +15,26 @@ import entity.Movie;
 public class FilterCategoryController {
 
     private final FilterCategorySelectionInputBoundary filterCategorySelectionUseCaseInteractor;
+    private final FilterApplicationInputBoundary filterApplicationUseCaseInteractor;
 
-    public FilterCategoryController(FilterCategorySelectionInputBoundary filterCategorySelectionUseCaseInteractor) {
+    public FilterCategoryController(FilterCategorySelectionInputBoundary filterCategorySelectionUseCaseInteractor,
+                                    FilterApplicationInputBoundary filterApplicationUseCaseInteractor) {
         this.filterCategorySelectionUseCaseInteractor = filterCategorySelectionUseCaseInteractor;
+        this.filterApplicationUseCaseInteractor = filterApplicationUseCaseInteractor;
+
     }
 
-    public void execute(String categoryName, String[] categoryOptions, List<Movie> originalList) {
+    public void executeFilterCategorySelection(String categoryName, String[] categoryOptions, List<Movie> originalList,
+                                               List<Movie> filteredList, List<String> selectedFilters) {
         final FilterCategorySelectionInputData filterCategorySelectionInputData = new FilterCategorySelectionInputData(
-                categoryName, categoryOptions, originalList);
+                categoryName, categoryOptions, originalList, filteredList, selectedFilters);
         filterCategorySelectionUseCaseInteractor.execute(filterCategorySelectionInputData);
+    }
+
+    public void executeFilterApplication(String categoryName, List<String> optionsSelected,
+                                         List<Movie> originalList) {
+        final FilterApplicationInputData filterApplicationInputData = new FilterApplicationInputData(categoryName,
+                optionsSelected, originalList);
+        filterApplicationUseCaseInteractor.execute(filterApplicationInputData);
     }
 }
--- src/main/java/interface_adapter/filter_category/FilterCategoryPresenter.java
+++ src/main/java/interface_adapter/filter_category/FilterCategoryPresenter.java
@@ -1,10 +1,12 @@
 package interface_adapter.filter_category;
 
 import interface_adapter.ViewManagerModel;
+import use_case.filter_application.FilterApplicationOutputBoundary;
+import use_case.filter_application.FilterApplicationOutputData;
 import use_case.filter_category_selection.FilterCategorySelectionOutputBoundary;
 import use_case.filter_category_selection.FilterCategorySelectionOutputData;
 
-public class FilterCategoryPresenter implements FilterCategorySelectionOutputBoundary {
+public class FilterCategoryPresenter implements FilterCategorySelectionOutputBoundary, FilterApplicationOutputBoundary {
 
     private FilterCategoryViewModel filterCategoryViewModel;
     private ViewManagerModel viewManagerModel;
@@ -21,11 +23,24 @@ public class FilterCategoryPresenter implements FilterCategorySelectionOutputBou
         //Set the appropriate instance variable values for the Filter Category State
         filterCategoryState.setCategoryName(filterCategorySelectionOutputData.getCategoryName());
         filterCategoryState.setCategoryOptions(filterCategorySelectionOutputData.getCategoryOptions());
+        filterCategoryState.setFilteredList(filterCategorySelectionOutputData.getFilteredList());
+        filterCategoryState.setOriginalList(filterCategorySelectionOutputData.getOriginalList());
         //Update the State in the Filter Category View Model.
         this.filterCategoryViewModel.setState(filterCategoryState);
         this.filterCategoryViewModel.firePropertyChanged();
         //Update the View Manager Model.
         this.viewManagerModel.setState(filterCategoryViewModel.getViewName());
         this.viewManagerModel.firePropertyChanged();
+    }
+
+    public void updateFilteredList(FilterApplicationOutputData filterApplicationOutputData) {
+        //get the current state of the Filter Category view model.
+        final FilterCategoryState filterCategoryState = filterCategoryViewModel.getState();
+        //Set the appropriate instance variable values for the Filter Category State
+        filterCategoryState.setFilteredList(filterApplicationOutputData.getApplicableMovies());
+        //Update the State in the Filter Category View Model.
+        this.filterCategoryViewModel.setState(filterCategoryState);
+        this.filterCategoryViewModel.firePropertyChanged();
+        //No need to update view model at this point; we are still on the same view.
     }
 }
