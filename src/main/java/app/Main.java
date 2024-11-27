5f37e904242857d9f03a74da6c9d5f7467197e4b
selincakir22 <selin.cakir@mail.utoronto.ca>
2024-11-26 21:30:34 -0500

    Using DBUserDataAccessObject to save user and user's info, such as fav movie and fav director.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -21,7 +21,6 @@ public class Main {
                                             .addMovieDetailView()
                                              .addUserProfileView()
                                             .addSignupUseCase()
-                                            .addSaveUserUseCase()
                                             .addLoginUseCase()
                                             .addLogoutUseCase()
                                             .addGoProfileUseCase()
